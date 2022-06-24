ee909fdb82b8498354f7e7a56858b0438fdc9939
Laura Ribeiro <lauradeor@gmail.com>
2022-06-23 23:25:20 -0300

    feat: first commit - portfolio in construction

---
+++ app-portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Em construção</p>
+        <h1>:-)</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
